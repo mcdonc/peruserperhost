b4aa558912bb43bc6357e8f4029fbb2f0bc16087
Chris McDonough <chrism@plope.com>
2024-03-22 19:38:40 -0400

    get home-manager ready for use

--- flake.nix
+++ flake.nix
@@ -3,22 +3,38 @@
 
     inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+      home-manager.url = "github:nix-community/home-manager/release-23.11";
     };
 
-    outputs = { self, nixpkgs }@inputs:
-      {
+    outputs = { self, home-manager, nixpkgs }@inputs:
+    let
+      system = "x86_64-linux";
+      specialArgs = inputs // { inherit system; };
+      shared-modules = [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useUserPackages = true;
+            extraSpecialArgs = specialArgs;
+          };
+        }
+      ];
+    in {
         nixosConfigurations = {
           host1 = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [ ./host1.nix ];
+            specialArgs = specialArgs;
+	    system = system;
+            modules = shared-modules ++ [ ./host1.nix ];
           };
           host2 = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [ ./host2.nix ];
+            specialArgs = specialArgs;
+	    system = system;
+            modules = shared-modules ++ [ ./host2.nix ];
           };
           host3 = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [ ./host3.nix ];
+            specialArgs = specialArgs;
+	    system = system;
+            modules = shared-modules ++ [ ./host3.nix ];
           };
         };
       };
