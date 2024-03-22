662d0802bec17ffa8257df1c12728554c14ce609
Chris McDonough <chrism@plope.com>
2024-03-22 06:47:21 -0400

    first commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+    description = "My flakes configuration";
+
+    inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    };
+
+    outputs = { self, nixpkgs }@inputs:
+      {
+        nixosConfigurations = {
+          host1 = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [ ./configuration.nix];
+          };
+        };
+      };
+}
