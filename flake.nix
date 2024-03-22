2400002eb468491e27408f990be13cae2db0efa3
Chris McDonough <chrism@plope.com>
2024-03-22 17:50:20 -0400

    add host3

--- flake.nix
+++ flake.nix
@@ -16,6 +16,10 @@
             system = "x86_64-linux";
             modules = [ ./host2.nix ];
           };
+          host3 = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [ ./host3.nix ];
+          };
         };
       };
 }
